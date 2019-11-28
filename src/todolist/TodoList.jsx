1caadb6804acea6182892b19b3bea8614662e667
222 <111>
2019-11-28 10:20:38 +0800

    try again

---
+++ src/todolist/TodoList.jsx
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+import Form from './Form'
+import List from './List'
+class TodoList extends Component {
+    render(){
+        return <Form></Form>
+    }
+}
+export default TodoList
